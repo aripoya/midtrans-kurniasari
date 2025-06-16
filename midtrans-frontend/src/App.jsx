b6323ca6e73d4666e556d6d705387311806e5da7
aripoya <wahwooh@gmail.com>
2025-06-16 13:12:33 +0700

    Restore full routing in App.jsx including /orders, /orders/new, and /orders/:id routes

--- midtrans-frontend/src/App.jsx
+++ midtrans-frontend/src/App.jsx
@@ -2,16 +2,10 @@ import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
-
-// Simple home page component 
-function HomePage() {
-  return (
-    <div>
-      <h1>Kurniasari Order Management</h1>
-      <p>Halaman Beranda Sederhana</p>
-    </div>
-  )
-}
+import HomePage from './pages/HomePage'
+import OrdersPage from './pages/OrdersPage'
+import NewOrderPage from './pages/NewOrderPage'
+import OrderDetailPage from './pages/OrderDetailPage'
 
 function App() {
   return (
@@ -20,6 +14,9 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders/:id" element={<OrderDetailPage />} />
           </Routes>
         </Layout>
       </Router>
